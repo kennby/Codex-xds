cf8372eac959b2664e0a51bd0a6b060a24397242
kennby <153956421+kennby@users.noreply.github.com>
2024-01-22 20:31:05 -0500

    Update menu-descargas.js

--- plugins/menu-descargas.js
+++ plugins/menu-descargas.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
   if (usedPrefix == 'a' || usedPrefix == 'A') return;
   try {
-      m.react('📥')
+      m.react('🖱️')
    const pp = imagen3;
     // let vn = './media/menu.mp3'
     const d = new Date(new Date + 3600000);
@@ -25,41 +25,41 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
 *╭───── • ◆ • ─────┅┅┄┄*
       *ᴅᴇsᴄᴀʀɢᴀs*
    
-▸🔰 _${usedPrefix}instagram *<enlace / link / url>*_
-▸🔰 _${usedPrefix}mediafire *<enlace / link / url>*_
-▸🔰 _${usedPrefix}gitclone *<enlace / link / url>*_
-▸🔰 _${usedPrefix}gdrive *<enlace / link / url>*_
-▸🔰 _${usedPrefix}tiktok *<enlace / link / url>*_
-▸🔰 _${usedPrefix}tiktokimg *<enlace / link / url>*_
-▸🔰 _${usedPrefix}twitter *<enlace / link / url>*_
-▸🔰 _${usedPrefix}fb *<enlace / link / url>*_
-▸🔰 _${usedPrefix}ytshort *<enlace / link / url>*_
-▸🔰 _${usedPrefix}ytmp3 *<enlace / link / url>*_
-▸🔰 _${usedPrefix}ytmp4 *<enlace / link / url>*_
-▸🔰 _${usedPrefix}ytmp3doc *<enlace / link / url>*_
-▸🔰 _${usedPrefix}ytmp4doc *<enlace / link / url>*_
-▸🔰 _${usedPrefix}videodoc *<enlace / link / url>*_
-▸🔰 _${usedPrefix}dapk2 *<enlace / link / url>*_
-▸🔰 _${usedPrefix}stickerpack *<enlace / link / url>*_
-▸🔰 _${usedPrefix}play *<texto>*_
-▸🔰 _${usedPrefix}play2 *<texto>*_
-▸🔰 _${usedPrefix}play.1 *<texto>*_
-▸🔰 _${usedPrefix}play.2 *<texto>*_
-▸🔰 _${usedPrefix}playdoc *<texto>*_
-▸🔰 _${usedPrefix}playdoc2 *<texto>*_
-▸🔰 _${usedPrefix}playlist *<texto>*_
-▸🔰 _${usedPrefix}spotify *<texto>*_
-▸🔰 _${usedPrefix}ringtone *<texto>*_
-▸🔰 _${usedPrefix}soundcloud *<texto>*_
-▸🔰 _${usedPrefix}imagen *<texto>*_
-▸🔰 _${usedPrefix}pinterest *<texto>*_
-▸🔰 _${usedPrefix}wallpaper *<texto>*_
-▸🔰 _${usedPrefix}pptiktok *<nombre de usuario>*_
-▸🔰 _${usedPrefix}igstalk *<nombre de usuario>*_
-▸🔰 _${usedPrefix}igstory *<nombre de usuario>*_
-▸🔰 _${usedPrefix}tiktokstalk *<username>*_
+▸✨ _${usedPrefix}instagram *<enlace / link / url>*_
+▸✨ _${usedPrefix}mediafire *<enlace / link / url>*_
+▸✨ _${usedPrefix}gitclone *<enlace / link / url>*_
+▸✨ _${usedPrefix}gdrive *<enlace / link / url>*_
+▸✨ _${usedPrefix}tiktok *<enlace / link / url>*_
+▸✨ _${usedPrefix}tiktokimg *<enlace / link / url>*_
+▸✨ _${usedPrefix}twitter *<enlace / link / url>*_
+▸✨ _${usedPrefix}fb *<enlace / link / url>*_
+▸✨ _${usedPrefix}ytshort *<enlace / link / url>*_
+▸✨ _${usedPrefix}ytmp3 *<enlace / link / url>*_
+▸✨ _${usedPrefix}ytmp4 *<enlace / link / url>*_
+▸✨ _${usedPrefix}ytmp3doc *<enlace / link / url>*_
+▸✨ _${usedPrefix}ytmp4doc *<enlace / link / url>*_
+▸✨ _${usedPrefix}videodoc *<enlace / link / url>*_
+▸✨ _${usedPrefix}dapk2 *<enlace / link / url>*_
+▸✨ _${usedPrefix}stickerpack *<enlace / link / url>*_
+▸✨ _${usedPrefix}play *<texto>*_
+▸✨ _${usedPrefix}play2 *<texto>*_
+▸✨ _${usedPrefix}play.1 *<texto>*_
+▸✨ _${usedPrefix}play.2 *<texto>*_
+▸✨ _${usedPrefix}playdoc *<texto>*_
+▸✨ _${usedPrefix}playdoc2 *<texto>*_
+▸✨ _${usedPrefix}playlist *<texto>*_
+▸✨ _${usedPrefix}spotify *<texto>*_
+▸✨ _${usedPrefix}ringtone *<texto>*_
+▸✨ _${usedPrefix}soundcloud *<texto>*_
+▸✨ _${usedPrefix}imagen *<texto>*_
+▸✨ _${usedPrefix}pinterest *<texto>*_
+▸✨ _${usedPrefix}wallpaper *<texto>*_
+▸✨ _${usedPrefix}pptiktok *<nombre de usuario>*_
+▸✨ _${usedPrefix}igstalk *<nombre de usuario>*_
+▸✨ _${usedPrefix}igstory *<nombre de usuario>*_
+▸✨ _${usedPrefix}tiktokstalk *<username>*_
 *╰───── • ◆ • ─────┅┅┄┄*
-*_BY: 𝗢𝗡𝗬𝗫_*`.trim();
+*_BY: 𝗜 𝗕𝗟𝗢𝗢𝗗 𝗕𝗢𝗧_*`.trim();
     if (m.isGroup) {
       // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
       const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};

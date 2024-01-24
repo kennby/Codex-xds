4e01e55949cccbae3b9b265ab75fd510b5f5be32
kennby <153956421+kennby@users.noreply.github.com>
2024-01-24 11:42:01 -0500

    Update descargas-tiktok.js

--- plugins/descargas-tiktok.js
+++ plugins/descargas-tiktok.js
@@ -12,7 +12,7 @@ const handler = async (m, {conn, text, args, usedPrefix, command}) => {
   // let buttons = [{ buttonText: { displayText: '♫ 𝙰𝚄𝙳𝙸𝙾 ♫' }, buttonId: `${usedPrefix}tomp3` }]
   try {
     const aa = {quoted: m, userJid: conn.user.jid};
-    const prep = generateWAMessageFromContent(m.chat, {extendedTextMessage: {text: texto, contextInfo: {externalAdReply: {title: '𝗜 𝗕𝗟𝗢𝗢𝗗 𝗕𝗢𝗧', body: null, thumbnail: imagen1, sourceUrl: 'https://whatsapp.com/channel/0029VaDajsjLI8YRnDjT2y1v'}, mentionedJid: [m.sender]}}}, aa);
+    const prep = generateWAMessageFromContent(m.chat, {extendedTextMessage: {text: texto, contextInfo: {externalAdReply: {title: '𝐜𝐨𝐝𝐞𝐱', body: null, thumbnail: imagen1, sourceUrl: 'https://whatsapp.com/channel/0029VaDajsjLI8YRnDjT2y1v'}, mentionedJid: [m.sender]}}}, aa);
     await conn.relayMessage(m.chat, prep.message, {messageId: prep.key.id, mentions: [m.sender]});
     const dataFn = await conn.getFile(`${CFROSAPI}/api/tiktokv2?url=${args[0]}`);
     const desc1n = `_*tiktok*_\n\n* Responde a este vídeo con el comando* _${usedPrefix}tomp3_ *para convertirlo en audio.*`;
@@ -20,7 +20,7 @@ const handler = async (m, {conn, text, args, usedPrefix, command}) => {
   } catch (ee1) {
   try {
     //const aa = {quoted: m, userJid: conn.user.jid};
-    //const prep = generateWAMessageFromContent(m.chat, {extendedTextMessage: {text: texto, contextInfo: {externalAdReply: {title: '𝗜 𝗕𝗟𝗢𝗢𝗗 𝗕𝗢𝗧', body: null, thumbnail: imagen1, sourceUrl: 'https://github.com/BrunoSobrino/TheMystic-Bot-MD'}, mentionedJid: [m.sender]}}}, aa);
+    //const prep = generateWAMessageFromContent(m.chat, {extendedTextMessage: {text: texto, contextInfo: {externalAdReply: {title: '𝐜𝐨𝐝𝐞𝐱', body: null, thumbnail: imagen1, sourceUrl: 'https://github.com/BrunoSobrino/TheMystic-Bot-MD'}, mentionedJid: [m.sender]}}}, aa);
     //await conn.relayMessage(m.chat, prep.message, {messageId: prep.key.id, mentions: [m.sender]});
     const dataF = await tiktok.v1(args[0]);
     // let desc1 =  `*𝙽𝙸𝙲𝙺𝙽𝙰𝙼𝙴:* ${dataF.nickname || 'Indefinido'}`
